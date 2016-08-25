2d69d4ca267061524f31ca84d2d992f5bd4f6db4
Domantas <dohxis@gmail.com>
2016-08-25 14:34:38 +0300

    All needed tokens has been added.

--- src/compiler.py
+++ src/compiler.py
@@ -6,6 +6,7 @@ class Program(object):
         self.pos = 0
         self.nodes = []
         self.tokens = []
+        self.nodes = []
         self.source = source
         self.init()
 
@@ -28,6 +29,59 @@ class Program(object):
                 self.incPos()
             return Token("DEFINITION", definition)
 
+        # Strings
+        if self.char() == '\"':
+            string = ""
+            self.incPos()
+            while self.char() != '\"':
+                string = string + self.char()
+                self.incPos()
+            return Token("STRING", string)
+
+        # Left paren
+        if self.char() == '(':
+            return Token("LPAREN", self.char())
+
+        # Right paren
+        if self.char() == ')':
+            return Token("RPAREN", self.char())
+
+        # Left curly
+        if self.char() == '{':
+            return Token("LCURLY", self.char())
+
+        # Right curly
+        if self.char() == '}':
+            return Token("RCURLY", self.char())
+
+        # Left bracket
+        if self.char() == '[':
+            return Token("LBRACKET", self.char())
+
+        # Right bracket
+        if self.char() == ']':
+            return Token("RBRACKET", self.char())
+
+        # Colon
+        if self.char() == ':':
+            return Token("COLON", self.char())
+
+        # Semicolon
+        if self.char() == ';':
+            return Token("SEMICOLON", self.char())
+
+        # Comma
+        if self.char() == ',':
+            return Token("COMMA", self.char())
+
+        # Equal
+        if self.char() == '=':
+            return Token("EQUAL", self.char())
+
+        # Binary operator
+        if self.char() in ['+', '-', '/', '*']:
+            return Token("BINARYOP", self.char())
+
         if self.char().isspace():
             return None
 
@@ -40,6 +94,9 @@ class Program(object):
                 self.tokens.append(token)
                 print(token)
             self.incPos()
+
+        print()
+
 
 def compile(source_file):
     with open(source_file, 'r') as source_file:
